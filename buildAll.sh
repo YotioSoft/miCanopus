de227b0715843e1e88836f172a1eba30604a7a10
YotioSoft <ytani0323@gmail.com>
2021-05-05 02:42:24 +0900

    PixelWriterの実装

--- buildAll.sh
+++ buildAll.sh
@@ -1,5 +1,8 @@
-source ./osbook/devenv/buildenv.sh
-cd ./kernel/
+BASEDIR="$HOME/git/miCanopus/osbook/devenv/x86_64-elf"
+export CPPFLAGS="-I$BASEDIR/include/c++/v1 -I$BASEDIR/include -I$BASEDIR/include/freetype2 -nostdlibinc -D__ELF__ -D_LDBL_EQ_DBL -D_GNU_SOURCE -D_POSIX_TIMERS"
+export LDFLAGS="-L$BASEDIR/lib"
+
+cd ~/git/miCanopus/kernel/
 make
 cd ~/edk2
 source edksetup.sh
